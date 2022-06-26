8f47b91fb475abd3d1d5501a371f4ba1f8eff8d2
hailang <hailang@webuy.ai>
2022-06-26 22:07:00 +0800

    add co status

---
+++ src/enums.rs
@@ -0,0 +1,16 @@
+pub enum CoroutineStatus {
+    ///协程被创建
+    Created,
+    ///等待运行
+    Ready,
+    ///运行中
+    Running,
+    ///被挂起
+    Suspend,
+    ///执行系统调用
+    SystemCall,
+    ///栈扩/缩容时
+    CopyStack,
+    ///已退出
+    Exited,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 /// 仅限框架内部使用的context
 pub(crate) mod context;
 
+pub mod enums;
+
 pub mod coroutine;
 
 /// Provides utilities to allocate memory suitable as stack memory for `Context`.
