61d5f9ac4e395ae6c65f62eaab69186cb7d906fe
hailang <a1936978077@hotmail.com>
2022-11-05 00:23:21 +0800

    初始化待实现的函数

--- libhook/src/lib.rs
+++ libhook/src/lib.rs
@@ -4,7 +4,168 @@ use std::os::raw::c_void;
 use std::ptr;
 use std::time::Duration;
 
-//被hook的系统函数
+/**
+被hook的系统函数
+#[no_mangle]避免rust编译器修改方法名称
+epoll like
+fcntl由于最后一个参数的类型问题，不支持
+todo 待支持io_uring
+todo 待完善编译条件
+ */
+#[no_mangle]
+pub fn poll(fds: *mut libc::pollfd, nfds: libc::nfds_t, timeout: libc::c_int) -> libc::c_int {
+    todo!()
+}
+
+#[no_mangle]
+pub fn select(
+    nfds: libc::c_int,
+    readfs: *mut libc::fd_set,
+    writefds: *mut libc::fd_set,
+    errorfds: *mut libc::fd_set,
+    timeout: *mut libc::timeval,
+) -> libc::c_int {
+    todo!()
+}
+
+#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos"))]
+#[no_mangle]
+pub fn kevent(
+    kq: libc::c_int,
+    changelist: *const libc::kevent,
+    nchanges: libc::c_int,
+    eventlist: *mut libc::kevent,
+    nevents: libc::c_int,
+    timeout: *const libc::timespec,
+) -> libc::c_int {
+    todo!()
+}
+
+#[cfg(linux)]
+#[no_mangle]
+pub fn epoll_wait(
+    epfd: libc::c_int,
+    events: *mut libc::epoll_event,
+    maxevents: libc::c_int,
+    timeout: libc::c_int,
+) -> libc::c_int {
+    todo!()
+}
+
+//socket相关
+#[no_mangle]
+pub fn setsockopt(
+    socket: libc::c_int,
+    level: libc::c_int,
+    name: libc::c_int,
+    value: *const libc::c_void,
+    option_len: libc::socklen_t,
+) -> libc::c_int {
+    todo!()
+}
+
+#[no_mangle]
+pub fn accept(
+    socket: libc::c_int,
+    address: *mut libc::sockaddr,
+    address_len: *mut libc::socklen_t,
+) -> libc::c_int {
+    //需要强制把socket设置为非阻塞
+    todo!()
+}
+
+#[no_mangle]
+pub fn connect(
+    socket: libc::c_int,
+    address: *const libc::sockaddr,
+    len: libc::socklen_t,
+) -> libc::c_int {
+    //需要强制把socket设置为非阻塞
+    todo!()
+}
+
+#[no_mangle]
+pub fn close(fd: libc::c_int) -> libc::c_int {
+    todo!()
+}
+
+//读数据
+#[no_mangle]
+pub fn read(fd: libc::c_int, buf: *mut libc::c_void, count: libc::size_t) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn readv(fd: libc::c_int, iov: *const libc::iovec, iovcnt: libc::c_int) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn recv(
+    socket: libc::c_int,
+    buf: *mut libc::c_void,
+    len: libc::size_t,
+    flags: libc::c_int,
+) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn recvfrom(
+    socket: libc::c_int,
+    buf: *mut libc::c_void,
+    len: libc::size_t,
+    flags: libc::c_int,
+    addr: *mut libc::sockaddr,
+    addrlen: *mut libc::socklen_t,
+) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn recvmsg(fd: libc::c_int, msg: *mut libc::msghdr, flags: libc::c_int) -> libc::ssize_t {
+    todo!()
+}
+
+//写数据
+#[no_mangle]
+pub fn write(fd: libc::c_int, buf: *const libc::c_void, count: libc::size_t) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn writev(fd: libc::c_int, iov: *const libc::iovec, iovcnt: libc::c_int) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn send(
+    socket: libc::c_int,
+    buf: *const libc::c_void,
+    len: libc::size_t,
+    flags: libc::c_int,
+) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn sendto(
+    socket: libc::c_int,
+    buf: *const libc::c_void,
+    len: libc::size_t,
+    flags: libc::c_int,
+    addr: *const libc::sockaddr,
+    addrlen: libc::socklen_t,
+) -> libc::ssize_t {
+    todo!()
+}
+
+#[no_mangle]
+pub fn sendmsg(fd: libc::c_int, msg: *const libc::msghdr, flags: libc::c_int) -> libc::ssize_t {
+    todo!()
+}
+
+//sleep相关
 #[no_mangle]
 pub extern "C" fn sleep(secs: libc::c_uint) -> libc::c_uint {
     let rqtp = libc::timespec {
@@ -69,7 +230,6 @@ pub extern "C" fn nanosleep(rqtp: *const libc::timespec, rmtp: *mut libc::timesp
     }
 }
 
-//#[no_mangle]避免rust编译器修改方法名称
 #[no_mangle]
 pub extern "C" fn coroutine_crate(pointer: &'static mut c_void) {
     let coroutine = unsafe {
